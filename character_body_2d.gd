53ca1fdd301be7b263f9175184613183b454d10d
Cui <57042254+CuiPixels@users.noreply.github.com>
2026-09-21 01:21:39 -0400

    Movement script and assets imported

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,13 +2,23 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var _animation_player = $AnimationPlayer
+
+
 func _physics_process(delta):
-	# Get input direction as a normalized vector
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("Up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("Left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("Right"):
+		velocity.x += 1
+	if Input.is_action_pressed("Down"):
+		velocity.y += 1
 	# Set velocity based on direction and speed
-	velocity = direction * speed
-	
-	# Move and handle collisions
-	move_and_slide()   
-	#^^^^ learn
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+
+# Move and handle collisions
+#move_and_slide()
